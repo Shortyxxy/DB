1914b8afa4e2d74d85f13d5f7a98de27e0d35236
AMB89 <AMB89@users.noreply.github.com>
2015-06-09 19:32:08 +0200

    Create DROP.sql

---
+++ DROP.sql
@@ -0,0 +1,16 @@
+drop table Beziehung_Kunde_Gesuch;
+drop table Beziehung_kunde_angebot;
+drop table Beziehung_Strecken;
+drop table Beziehung_Vermittlung;
+drop table angebot;
+drop table gesuch;
+drop table auto;
+drop table mitarbeiter;
+drop table kunde;
+drop table ort;
+
+
+DROP sequence Mitarbeiterseq;
+DROP sequence Kundenseq ;
+DROP sequence Gesuchseq;
+DROP sequence Angebotseq;
