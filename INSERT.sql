74277954c3a0d6e454123f10f98135cf03a85bbb
AMB89 <AMB89@users.noreply.github.com>
2015-06-09 19:34:05 +0200

    Create INSERT.sql

---
+++ INSERT.sql
@@ -0,0 +1,50 @@
+insert into ort (name) Values ('Augsburg'); 
+insert into ort (name) Values ('Muenchen');
+insert into ort (name) Values ('Kaeln');
+insert into ort (name) Values ('Bonn');
+insert into ort (name) Values ('Dortmund');
+insert into ort (name) Values ('Hannover');
+insert into ort (name) Values ('Frankfurt');
+insert into ort (name) Values ('Bremen');
+insert into ort (name) Values ('Koblenz');
+insert into ort (name) Values ('Kassel');
+insert into ort (name) Values ('Hamburg');
+insert into ort (name) Values ('Heidelberg');
+insert into ort (name) Values ('Nuernberg');
+insert into ort (name) Values ('Stuttgart');
+insert into ort (name) Values ('Ulm');
+
+insert into Mitarbeiter Values (1717234, 'Hans Mueller', 'Clauzenfeld 17', '68162', 'Mannheim' , '0621/237641', 'Bueroangestelter' );
+insert into Mitarbeiter Values (1737255, 'Anna Borreda', 'Muehlenweg 7', '61557', 'Oftersheim', '06223/66786', 'Schichtleiterin' );
+insert into Mitarbeiter Values (1137327, 'Franz Hoerner', 'Geranienstrasse 7', '68133', 'Mannheim','0621/6652526', 'Bueroangestelter' );
+
+insert into kunde
+Values ( kundenseq.nextval ,'Hans' , 'Meier' , 'Muehlingsstr 13'  , '68145' , 'Niederhockenstadt', '06447/31445');   
+insert into kunde
+Values (kundenseq.nextval,'Hubert' , 'Mahlrumpf' , 'Kirchweg 17' , '45889' , 'Kirchheim', '03445/5617');
+insert into kunde
+Values (kundenseq.nextval,'Frauke' , 'Keller' , 'Schwedenkai 1' , '23435' , 'Kiel', '02015/7617');
+insert into kunde
+Values (kundenseq.nextval,'Uli' , 'Knauf' , 'Hauptstr. 15' , '69411' , 'Wiesloch', '06222/62552');
+insert into kunde
+Values (kundenseq.nextval,'Fiederike' , 'Mueller' , 'Speyererstr. 7' , '69133' , 'Heidelberg', '06221/988457');
+insert into kunde
+Values (kundenseq.nextval,'Franz' , 'Fritz' , 'Walzwerkstr. 13' , '65123' , 'Worms', '06231/728282');
+insert into kunde
+Values (kundenseq.nextval,'Franziska' , 'Meier' , 'Muehlingsstr 13' , '68145' , 'Niederhockenstadt', '06447/31445'); 
+insert into kunde
+Values (kundenseq.nextval,'Franz' , 'Muetzenmeier' , 'Hauptstr. 15' , '69411' , 'Wiesloch', '06222/67761');
+insert into kunde
+Values (kundenseq.nextval,'Hans' , 'Schindler' , 'Muehlweg 17' , '41778' , 'Heltersberg', '06327/456171');
+insert into kunde
+Values (kundenseq.nextval,'Elisa' , 'Knuff' , 'Geranienstrasse 7' , '98445' , 'Bamberg', '09887/52526');
+insert into kunde
+Values (kundenseq.nextval,'Franz' , 'Bremkauf' , 'Knaufweg 15' , '98770' , 'Buxtehude', '01566/75917');
+
+insert into Auto Values ('Gruene Ford Transit',6,'DA - XL 9',1);
+insert into Auto Values ('Blauer VW Golf',5,'BOR - H 9987',2);
+insert into Auto Values ('Weiaees Mercedes Kombi',5,'DA - MH 312',1);
+insert into Auto Values ('Blauer Renault Twingo',5,'KI - T 556 ',3);
+insert into Auto Values ('Schwarzer Porsche',5,'HD - JJ 119',4);
+insert into Auto Values ('Schwarzer Porsche',5,'HD - UT 86',5);
+insert into Auto Values ('Verrosteter Renault R4',5,'WO - KJ 907',6);
