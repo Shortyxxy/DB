b140b505f84470ea93d13a775a139ff459217694
Kevin Grygosch <kevin_gry@gmx.de>
2015-06-10 11:58:31 +0200

    Update Drop 10_06

--- DROP.sql
+++ DROP.sql
@@ -1,12 +1,15 @@
+DROP TABLE Beziehung_Auto_Kunde;
 drop table Beziehung_Kunde_Gesuch;
 drop table Beziehung_kunde_angebot;
 drop table Beziehung_Strecken;
 drop table Beziehung_Vermittlung;
 drop table angebot;
 drop table gesuch;
+drop table kunde;
+
 drop table auto;
 drop table mitarbeiter;
-drop table kunde;
+
 drop table ort;
 
 
