6dab4f8878d2d0b460c43eabbe33898a6bb8d073
Kevin Grygosch <kevin_gry@gmx.de>
2015-06-14 22:49:56 +0200

    Update Drop

--- DROP.sql
+++ DROP.sql
@@ -1,17 +1,15 @@
+--DROP SCRIPT:
+
 DROP TABLE Beziehung_Auto_Kunde;
-drop table Beziehung_Kunde_Gesuch;
-drop table Beziehung_kunde_angebot;
+DROP TABLE parameter;
 drop table Beziehung_Strecken;
 drop table Beziehung_Vermittlung;
 drop table angebot;
 drop table gesuch;
 drop table kunde;
-
 drop table auto;
 drop table mitarbeiter;
-
 drop table ort;
-
 
 DROP sequence Mitarbeiterseq;
 DROP sequence Kundenseq ;
