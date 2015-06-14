2d31aa56c37a3df13aa40b022a963884623b2deb
Kevin Grygosch <kevin_gry@gmx.de>
2015-06-14 22:50:55 +0200

    Update Insert

--- INSERT.sql
+++ INSERT.sql
@@ -1,3 +1,5 @@
+--INSERT SCRIPT:
+
 insert into ort (name) Values ('Augsburg');
 insert into ort (name) Values ('Muenchen');
 insert into ort (name) Values ('Koeln');
@@ -15,9 +17,9 @@ insert into ort (name) Values ('Stuttgart');
 insert into ort (name) Values ('Ulm');
 insert into ort (name) Values ('Mannheim');
 
-insert into Mitarbeiter Values (1717234, 'Hans',' Mueller', 'Clauzenfeld 17', '68162', 'Mannheim' , '0621/237641', 'Bueroangestelter' );
-insert into Mitarbeiter Values (1737255, 'Anna' ,'Borreda', 'Muehlenweg 7', '61557', 'Oftersheim', '06223/66786', 'Schichtleiterin' );
-insert into Mitarbeiter Values (1137327, 'Franz',' Hoerner', 'Geranienstrasse 7', '68133', 'Mannheim','0621/6652526', 'Bueroangestelter' );
+insert into Mitarbeiter Values (1717234,'Mueller','Hans', 'Clauzenfeld 17', '68162', 'Mannheim' , '0621/237641', 'Bueroangestelter' );
+insert into Mitarbeiter Values (1737255, 'Borreda','Anna' , 'Muehlenweg 7', '61557', 'Oftersheim', '06223/66786', 'Schichtleiterin' );
+insert into Mitarbeiter Values (1137327,' Hoerner','Franz', 'Geranienstrasse 7', '68133', 'Mannheim','0621/6652526', 'Bueroangestelter' );
 
 insert into kunde
 Values ( kundenseq.nextval ,'Hans' , 'Meier' , 'Muehlingsstr 13'  , '68145' , 'Niederhockenstadt', '06447/31445');
@@ -45,13 +47,13 @@ Values (kundenseq.nextval,'Franz' , 'Bremkauf' , 'Knaufweg 15' , '98770' , 'Buxt
 insert into Auto Values ('Gruene Ford Transit',6,'DA - XL 9');
 insert into Auto Values ('Blauer VW Golf',5,'BOR - H 9987');
 insert into Auto Values ('Weisses Mercedes Kombi',5,'DA - MH 312');
-insert into Auto Values ('Blauer Renault Twingo',5,'KI - T 556 ');
+insert into Auto Values ('Blauer Renault Twingo',5,'KI - T 556');
 insert into Auto Values ('Schwarzer Porsche',5,'HD - JJ 119');
 insert into Auto Values ('Schwarzer Porsche',5,'HD - UT 86');
 insert into Auto Values ('Verrosteter Renault R4',5,'WO - KJ 907');
 
 insert into Gesuch (suchender ,gesuchnr ,ort_start ,ort_ziel ,Abfahrt,
-gesuchte_plaetze ,bemerkung ,Vermittlungsgebuehr,erfasst_von ,erfasst_am ) VALUES (1,Gesuchseq.nextval, 'Augsburg','Muenchen',
+gesuchte_plaetze ,bemerkung ,Vermittlungsgebuehr,erfasst_von ,erfasst_am ) VALUES (8,Gesuchseq.nextval, 'Augsburg','Muenchen',
 TO_DATE( '5-5-15', 'DD-MM-YY'),2,'Abhohlung am liebsten Augsburg Hbf','bezhalt',1137327,TO_DATE( '2-5-15', 'DD-MM-YY') );
 insert into Gesuch (suchender ,gesuchnr ,ort_start ,ort_ziel ,Abfahrt,
 gesuchte_plaetze,erfasst_von ,erfasst_am ) VALUES (9,Gesuchseq.nextval, 'Mannheim','Dortmund',
@@ -76,10 +78,10 @@ gesuchte_plaetze,erfasst_von ,erfasst_am ) VALUES (3,Gesuchseq.nextval, 'Stuttga
 TO_DATE( '23-6-15 12:00', 'DD-MM-YY HH24:MI'),TO_DATE( '23-6-15 18:00', 'DD-MM-YY HH24:MI'),2,1717234,TO_DATE( '20-6-15', 'DD-MM-YY') );
 
 
-insert into Angebot (Bietender,AngebotNr,Ort_Start,Ort_Ziel,Abfahrt,
-Gebotene_Plaetze,Treffpunkt,Bemerkung,Erfasst_von,Erfasst_am) VALUES (1,Angebotseq.nextval,'Mannheim','Muenchen',TO_DATE( '5-5-15 8:00', 'DD-MM-YY HH24:MI'),
+insert into Angebot (Bietender,AngebotNr,Ort_Start,Ort_Ziel,Ort_ueber,Abfahrt,
+Gebotene_Plaetze,Treffpunkt,Bemerkung,Erfasst_von,Erfasst_am) VALUES (1,Angebotseq.nextval,'Mannheim','Muenchen','Augsburg',TO_DATE( '5-5-15 8:00', 'DD-MM-YY HH24:MI'),
 2, 'Mannheim, ADAC-Stand Planetarium','Abholung Augsburg Hbf ok!, ca. 11:00 Uhr',1717234, TO_DATE( '1-5-15', 'DD-MM-YY'));
-insert into Angebot (Bietender,AngebotNr,Ort_Start,Ort_Ziel,Fruehste_Startzeit,Spateste_Startzeit,
+insert into Angebot (Bietender,AngebotNr,Ort_Start,Ort_Ziel,Fruehste_Startzeit,Spaeteste_Startzeit,
 Gebotene_Plaetze,Treffpunkt,Erfasst_von,Erfasst_am) VALUES (2,Angebotseq.nextval,'Mannheim','Dortmund',TO_DATE( '29-5-15 12:00', 'DD-MM-YY HH24:MI'),
 TO_DATE( '30-5-15 12:00', 'DD-MM-YY HH24:MI'),2, 'Mannheim, Wasserturm',1137327, TO_DATE( '15-5-15', 'DD-MM-YY'));
 insert into Angebot (Bietender,AngebotNr,Ort_Start,Ort_Ziel,Abfahrt,
@@ -107,25 +109,25 @@ insert into Angebot (Bietender,AngebotNr,Ort_Start,Ort_Ziel,Abfahrt,
 Gebotene_Plaetze,Treffpunkt,Erfasst_von,Erfasst_am) VALUES (7,Angebotseq.nextval,'Hannover','Bremen',TO_DATE( '30-6-15 9:00', 'DD-MM-YY HH24:MI'),
 1, 'Hannover, Linie 1, Fahrtrichtung Messe, Haltestelle Rotenbaumchaussee ',1717234, TO_DATE( '25-6-15', 'DD-MM-YY'));
 
-insert into Beziehung_Kunde_Gesuch VALUES (1,7);
-insert into Beziehung_Kunde_Gesuch VALUES (2,9);
-insert into Beziehung_Kunde_Gesuch VALUES (3,10);
-insert into Beziehung_Kunde_Gesuch VALUES (4,8);
-insert into Beziehung_Kunde_Gesuch VALUES (5,11);
-insert into Beziehung_Kunde_Gesuch VALUES (6,8);
-insert into Beziehung_Kunde_Gesuch VALUES (7,10);
-insert into Beziehung_Kunde_Gesuch VALUES (8,11);
+--insert into Beziehung_Kunde_Gesuch VALUES (1,7);
+--insert into Beziehung_Kunde_Gesuch VALUES (2,9);
+--insert into Beziehung_Kunde_Gesuch VALUES (3,10);
+--insert into Beziehung_Kunde_Gesuch VALUES (4,8);
+--insert into Beziehung_Kunde_Gesuch VALUES (5,11);
+--insert into Beziehung_Kunde_Gesuch VALUES (6,8);
+--insert into Beziehung_Kunde_Gesuch VALUES (7,10);
+--insert into Beziehung_Kunde_Gesuch VALUES (8,11);
 
-insert into Beziehung_Kunde_Angebot VALUES (1,1);
-insert into Beziehung_Kunde_Angebot VALUES (2,2);
-insert into Beziehung_Kunde_Angebot VALUES (3,1);
-insert into Beziehung_Kunde_Angebot VALUES (4,3);
-insert into Beziehung_Kunde_Angebot VALUES (5,4);
-insert into Beziehung_Kunde_Angebot VALUES (6,1);
-insert into Beziehung_Kunde_Angebot VALUES (7,3);
-insert into Beziehung_Kunde_Angebot VALUES (8,5);
-insert into Beziehung_Kunde_Angebot VALUES (9,6);
-insert into Beziehung_Kunde_Angebot VALUES (10,7);
+--insert into Beziehung_Kunde_Angebot VALUES (1,1);
+--insert into Beziehung_Kunde_Angebot VALUES (2,2);
+--insert into Beziehung_Kunde_Angebot VALUES (3,1);
+--insert into Beziehung_Kunde_Angebot VALUES (4,3);
+--insert into Beziehung_Kunde_Angebot VALUES (5,4);
+--insert into Beziehung_Kunde_Angebot VALUES (6,1);
+--insert into Beziehung_Kunde_Angebot VALUES (7,3);
+--insert into Beziehung_Kunde_Angebot VALUES (8,5);
+--insert into Beziehung_Kunde_Angebot VALUES (9,6);
+--insert into Beziehung_Kunde_Angebot VALUES (10,7);
 
 insert into Beziehung_Strecken VALUES (68, 'Augsburg', 'Muenchen');
 insert into Beziehung_Strecken VALUES (27, 'Bonn', 'Koeln');
@@ -157,9 +159,26 @@ insert into Beziehung_Strecken VALUES (222, 'Nuernberg', 'Frankfurt');
 insert into Beziehung_Strecken VALUES (92, 'Stuttgart', 'Ulm');
 insert into Beziehung_Strecken VALUES (81, 'Ulm', 'Augsburg');
 
-INSERT INTO Beziehung_Vermittlung (AngebotNr,GesuchNr,Fahrer,Mitfahrer,Fahrt_duerchgefuehrt,GEBUEHR,Bezahlt_am,Bezahlt_bei,vermittelt_von,vermittelt_am)
+INSERT INTO Beziehung_Vermittlung (AngebotNr,GesuchNr,Fahrer,Mitfahrer,Fahrt_durchgefuehrt,GEBUEHR,Bezahlt_am,Bezahlt_bei,vermittelt_von,vermittelt_am)
 VALUES  (1,1,1,8,1,'bezahlt',TO_DATE( '4-5-15', 'DD-MM-YY'),1717234,1717234,TO_DATE( '2-5-15', 'DD-MM-YY') );
-INSERT INTO Beziehung_Vermittlung (AngebotNr,GesuchNr,Fahrer,Mitfahrer,Fahrt_duerchgefuehrt,GEBUEHR,Bezahlt_am,Bezahlt_bei,vermittelt_von,vermittelt_am)
+INSERT INTO Beziehung_Vermittlung (AngebotNr,GesuchNr,Fahrer,Mitfahrer,Fahrt_durchgefuehrt,GEBUEHR,Bezahlt_am,Bezahlt_bei,vermittelt_von,vermittelt_am)
 VALUES  (3,2,1,9,0,'bezahlt',TO_DATE( '29-5-15', 'DD-MM-YY'),1717234,1137327,TO_DATE( '27-5-15', 'DD-MM-YY') );
-INSERT INTO Beziehung_Vermittlung (AngebotNr,GesuchNr,Fahrer,Mitfahrer,Fahrt_duerchgefuehrt,vermittelt_von,vermittelt_am)
-VALUES  (4,3,3,10,0,1717234,TO_DATE( '30-5-15', 'DD-MM-YY') );
+INSERT INTO Beziehung_Vermittlung (AngebotNr,GesuchNr,Fahrer,Mitfahrer,Fahrt_durchgefuehrt,vermittelt_von,vermittelt_am)
+VALUES  (4,3,3,10,0,1717234,TO_DATE( '30-5-15', 'DD-MM-YY') ) ;
+
+
+INSERT INTO Beziehung_Auto_Kunde VALUES (1, 'DA - XL 9' );
+INSERT INTO Beziehung_Auto_Kunde VALUES (2, 'BOR - H 9987' );
+INSERT INTO Beziehung_Auto_Kunde VALUES (1, 'DA - MH 312' );
+INSERT INTO Beziehung_Auto_Kunde VALUES (3, 'KI - T 556' );
+INSERT INTO Beziehung_Auto_Kunde VALUES (4, 'HD - JJ 119' );
+INSERT INTO Beziehung_Auto_Kunde VALUES (5, 'HD - UT 86' );
+INSERT INTO Beziehung_Auto_Kunde VALUES (6, 'WO - KJ 907' );
+INSERT INTO Beziehung_Auto_Kunde VALUES (7, 'DA - MH 312' );
+
+
+
+
+
+
+insert into Parameter VALUES (0.08, 3, 10)
